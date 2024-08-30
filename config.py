6817efc315bd395685b5a078cbcab63f5a6846f0
btmkr2024 <btmaker2020@yahoo.com>
2024-08-30 18:05:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "27868202"))
+API_HASH = getenv("API_HASH", "bf6d423ddc4a4999f09750872bdc2497")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6497588567").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://adultverse04:adultverse04@adultverse04.wpltj.mongodb.net/?retryWrites=true&w=majority&appName=adultverse04")
+LOG_GROUP = getenv("LOG_GROUP", "-1002224815385")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002232787597"))
